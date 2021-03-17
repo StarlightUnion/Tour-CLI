ea608d49ee11b1327ead0ca6eb6a9a649d99f300
wangxingchen <2387574095@qq.com>
2021-03-17 09:38:50 +0800

    publish: 0.0.3-beta

--- README.md
+++ README.md
@@ -1,2 +1,15 @@
 # Tust-CLI
 脚手架DIY
+
+## 安装
+
+```shell
+npm i tust-cli -g
+```
+
+## 使用
+
+```shell
+tust-cli
+```
+
--- utils/colorCli.ts
+++ utils/colorCli.ts
@@ -2,7 +2,7 @@
  * @Description: 处理命令行颜色
  * @Author: tourist17846
  * @Date: 2021-03-16 23:12:51
- * @LastEditTime: 2021-03-16 23:46:06
+ * @LastEditTime: 2021-03-17 08:41:27
  */
 
 import * as chalk from 'chalk';
